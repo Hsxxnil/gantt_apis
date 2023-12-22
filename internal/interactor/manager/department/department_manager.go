c1cc87f1478e2e44cf6099d1c3a85c08dd49c983
Hsxxnil <lhsuan1225@gmail.com>
2023-12-22 16:52:09 +0800

    feat: delete & update department sync handle affiliation

--- internal/interactor/manager/department/department_manager.go
+++ internal/interactor/manager/department/department_manager.go
@@ -3,7 +3,9 @@ package department
 import (
 	"errors"
 	"github.com/bytedance/sonic"
+	affiliationModel "hta/internal/interactor/models/affiliations"
 	"hta/internal/interactor/pkg/util"
+	affiliationService "hta/internal/interactor/service/affiliation"
 
 	"gorm.io/gorm"
 
@@ -19,17 +21,19 @@ type Manager interface {
 	GetByList(input *departmentModel.Fields) (int, any)
 	GetByListNoPagination(input *departmentModel.Field) (int, any)
 	GetBySingle(input *departmentModel.Field) (int, any)
-	Delete(input *departmentModel.Field) (int, any)
-	Update(input *departmentModel.Update) (int, any)
+	Delete(trx *gorm.DB, input *departmentModel.Field) (int, any)
+	Update(trx *gorm.DB, input *departmentModel.Update) (int, any)
 }
 
 type manager struct {
-	DepartmentService departmentService.Service
+	DepartmentService  departmentService.Service
+	AffiliationService affiliationService.Service
 }
 
 func Init(db *gorm.DB) Manager {
 	return &manager{
-		DepartmentService: departmentService.Init(db),
+		DepartmentService:  departmentService.Init(db),
+		AffiliationService: affiliationService.Init(db),
 	}
 }
 
@@ -40,6 +44,20 @@ func (m *manager) Create(trx *gorm.DB, input *departmentModel.Create) (int, any)
 	if err != nil {
 		log.Error(err)
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+	}
+
+	// sync create affiliation
+	if input.SupervisorID != nil {
+		_, err = m.AffiliationService.WithTrx(trx).Create(&affiliationModel.Create{
+			UserID:       *input.SupervisorID,
+			DeptID:       *departmentBase.ID,
+			IsSupervisor: true,
+			CreatedBy:    input.CreatedBy,
+		})
+		if err != nil {
+			log.Error(err)
+			return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+		}
 	}
 
 	trx.Commit()
@@ -136,7 +154,9 @@ func (m *manager) GetBySingle(input *departmentModel.Field) (int, any) {
 	return code.Successful, code.GetCodeMessage(code.Successful, output)
 }
 
-func (m *manager) Delete(input *departmentModel.Field) (int, any) {
+func (m *manager) Delete(trx *gorm.DB, input *departmentModel.Field) (int, any) {
+	defer trx.Rollback()
+
 	_, err := m.DepartmentService.GetBySingle(&departmentModel.Field{
 		ID: input.ID,
 	})
@@ -149,16 +169,28 @@ func (m *manager) Delete(input *departmentModel.Field) (int, any) {
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
-	err = m.DepartmentService.Delete(input)
+	err = m.DepartmentService.WithTrx(trx).Delete(input)
 	if err != nil {
 		log.Error(err)
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
+	// sync delete affiliation
+	err = m.AffiliationService.WithTrx(trx).Delete(&affiliationModel.Field{
+		DeptID: util.PointerString(input.ID),
+	})
+	if err != nil {
+		log.Error(err)
+		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+	}
+
+	trx.Commit()
 	return code.Successful, code.GetCodeMessage(code.Successful, "Delete ok!")
 }
 
-func (m *manager) Update(input *departmentModel.Update) (int, any) {
+func (m *manager) Update(trx *gorm.DB, input *departmentModel.Update) (int, any) {
+	defer trx.Rollback()
+
 	departmentBase, err := m.DepartmentService.GetBySingle(&departmentModel.Field{
 		ID: input.ID,
 	})
@@ -171,11 +203,51 @@ func (m *manager) Update(input *departmentModel.Update) (int, any) {
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
-	err = m.DepartmentService.Update(input)
+	err = m.DepartmentService.WithTrx(trx).Update(input)
 	if err != nil {
 		log.Error(err)
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
+	if input.SupervisorID != nil {
+		// check the original supervisor exist
+		originalAffiliationBase, err := m.AffiliationService.GetBySingle(&affiliationModel.Field{
+			DeptID:       util.PointerString(input.ID),
+			IsSupervisor: util.PointerBool(true),
+		})
+		if err != nil {
+			if !errors.Is(err, gorm.ErrRecordNotFound) {
+				log.Error(err)
+				return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+			}
+		}
+
+		if originalAffiliationBase != nil {
+			// update the original supervisor's affiliation
+			err = m.AffiliationService.WithTrx(trx).Update(&affiliationModel.Update{
+				ID:           *originalAffiliationBase.ID,
+				IsSupervisor: util.PointerBool(false),
+				UpdatedBy:    input.UpdatedBy,
+			})
+			if err != nil {
+				log.Error(err)
+				return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+			}
+		}
+
+		// sync update the new supervisor's affiliation
+		err = m.AffiliationService.WithTrx(trx).Update(&affiliationModel.Update{
+			DeptID:       util.PointerString(input.ID),
+			UserID:       input.SupervisorID,
+			IsSupervisor: util.PointerBool(true),
+			UpdatedBy:    input.UpdatedBy,
+		})
+		if err != nil {
+			log.Error(err)
+			return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
+		}
+	}
+
+	trx.Commit()
 	return code.Successful, code.GetCodeMessage(code.Successful, departmentBase.ID)
 }
