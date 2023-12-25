bf676cce1cec7020ee036adfac4973e25517f7e0
Hsxxnil <lhsuan1225@gmail.com>
2023-12-25 18:35:57 +0800

    refactor: code refactoring

--- internal/interactor/manager/user/user_manager.go
+++ internal/interactor/manager/user/user_manager.go
@@ -378,7 +378,6 @@ func (m *manager) Enable(input *userModel.Enable) (int, any) {
 		return code.InternalServerError, code.GetCodeMessage(code.InternalServerError, err.Error())
 	}
 
-	log.Debug("update: ", update.IsEnabled)
 	err = m.UserService.Update(update)
 	if err != nil {
 		log.Error(err)
