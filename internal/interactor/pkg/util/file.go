d6f77b0957dace2fe7ab6c223156254c16e0f368
Hsxxnil <lhsuan1225@gmail.com>
2024-01-30 13:35:12 +0800

    fix: config add s3 bucket name

--- internal/interactor/pkg/util/file.go
+++ internal/interactor/pkg/util/file.go
@@ -4,14 +4,15 @@ import (
 	"encoding/base64"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"hta/config"
 	s3pkg "hta/internal/interactor/pkg/aws/s3"
 	"hta/internal/interactor/pkg/util/log"
 	"strings"
 )
 
 // UploadToS3 is used to upload file to s3
-func UploadToS3(inputBase64, filePath string, s3BucketName string) (url string, err error) {
-	sg := s3pkg.NewAmazonStorage(s3BucketName)
+func UploadToS3(inputBase64, filePath string) (url string, err error) {
+	sg := s3pkg.NewAmazonStorage(config.S3BucketName)
 	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(inputBase64))
 	object := &s3.PutObjectInput{
 		Key:  aws.String(filePath),
