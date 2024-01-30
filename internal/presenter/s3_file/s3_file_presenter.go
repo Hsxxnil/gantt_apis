5fa37fb6c49c0f3ce7c3aba823378dacbcbf35d7
Hsxxnil <lhsuan1225@gmail.com>
2024-01-30 15:04:44 +0800

    fix: multiple file upload

--- internal/presenter/s3_file/s3_file_presenter.go
+++ internal/presenter/s3_file/s3_file_presenter.go
@@ -35,7 +35,7 @@ func Init(db *gorm.DB) Control {
 // @Accept json
 // @produce json
 // @param Authorization header string true "JWE Token"
-// @param * body s3_files.Create true "上傳檔案"
+// @param * body []s3_files.Create true "上傳檔案"
 // @success 200 object code.SuccessfulMessage{body=string} "成功後返回的值"
 // @failure 415 object code.ErrorMessage{detailed=string} "必要欄位帶入錯誤"
 // @failure 500 object code.ErrorMessage{detailed=string} "伺服器非預期錯誤"
